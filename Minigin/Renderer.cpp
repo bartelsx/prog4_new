e944daa8a88f6df4a953b1c1bc4c7d92e975ed07
bartelsx <bartelsx@gmail.com>
2023-03-15 14:48:03 +0100

    ImGUI added

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -1,5 +1,10 @@
 #include <stdexcept>
 #include "Renderer.h"
+
+#include "imgui.h"
+
+#include "imgui_impl_opengl2.h"
+#include "imgui_impl_sdl.h"
 #include "SceneManager.h"
 #include "Texture2D.h"
 
@@ -25,6 +30,10 @@ void dae::Renderer::Init(SDL_Window* window)
 	{
 		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
 	}
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplSDL2_InitForOpenGL(window, SDL_GL_GetCurrentContext());
+	ImGui_ImplOpenGL2_Init();
 }
 
 void dae::Renderer::Render() const
@@ -34,12 +43,23 @@ void dae::Renderer::Render() const
 	SDL_RenderClear(m_renderer);
 
 	SceneManager::GetInstance().Render();
-	
+
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(m_window);
+	ImGui::NewFrame();
+	ImGui::ShowDemoWindow();
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
 	SDL_RenderPresent(m_renderer);
 }
 
 void dae::Renderer::Destroy()
 {
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+	
 	if (m_renderer != nullptr)
 	{
 		SDL_DestroyRenderer(m_renderer);
